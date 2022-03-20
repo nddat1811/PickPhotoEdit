eb4556c0de55ccf79f85a37baeabc0b00d49cdfa
nddat1811 <nddat1811@gmail.com>
2022-03-20 19:15:50 +0700

    oke done

--- app/src/main/java/com/ts/photopicker/MainActivity.java
+++ app/src/main/java/com/ts/photopicker/MainActivity.java
@@ -30,14 +30,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkPermission() {
-        //Initialize permission
+        //Initialize permission    ở đây khởi tạo biến cho phép để chỉnh sửa ảnh trong thư mục á, gradle có user-permiss rồi, này call ra thôi
         int permission = ActivityCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE);
         //Check condition
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){ //Q laf 29 R laf 28 ???
+            // này t chưa hiểu lắm :v cái điều kiện này không hiểu làm gì
+
             //When device version is greater than equal to version 10
             //Create method
-            pickImage();
+            pickImage();  // chọn ảnh
         } else {
             //When device version is below version 10
             //Check condition
@@ -57,11 +59,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void pickImage() {
         //Initialize Intent
-        Intent intent = new Intent(Intent.ACTION_PICK);
+        Intent intent = new Intent(Intent.ACTION_PICK);  // Nó tạo 1 cái Intent mới với action là pick
         //Set type
-        intent.setType("image/*");
+        intent.setType("image/*");  //Chọn các loại intent là ảnh
         //Start activity for result
-        startActivityForResult(intent, 100);
+        startActivityForResult(intent, 100); //này t ghi theo thôi chứ cũng chưa hiểu :v
     }
 
     @Override
@@ -87,18 +89,18 @@ public class MainActivity extends AppCompatActivity {
         //Check condition
         if (resultCode == RESULT_OK) {
             //When result is ok initialize uri
-            Uri uri = data.getData();
+            Uri uri = data.getData();  //Này là lấy uri của cái ảnh dc pick
             switch (requestCode) {
                 case 100:
                     //When request code is equal to 100
                     //Initialize intent
-                    Intent intent = new Intent(MainActivity.this, EditPicture.class);
+                    Intent intent = new Intent(MainActivity.this, EditPicture.class);   //Tạo intent mới với
                     //Set data
                     Bundle bundle = new Bundle();
                     Log.d("img_data", data.getData().toString());
-                    bundle.putString("img_data", uri.toString());
+                    bundle.putString("img_data", uri.toString());    // chuyển uri thành string rồi chuyển qua activity 2 á
                     intent.putExtras(bundle);
-                    startActivity(intent);
+                    startActivity(intent);          //Chuyển qua màn hình edit... bên edit chủ yếu mới show ra á, chưa làm gì ehét
                     //Set output directory name
 
             }
