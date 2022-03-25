7f147e3f3c896f2bbafa72d1f0a4eb8d60cfe78c
nddat1811 <nddat1811@gmail.com>
2022-03-25 15:43:48 +0700

    :<<<<<<<

--- TunJave/app/src/main/java/com/ts/tunjave/MainActivity.java
+++ TunJave/app/src/main/java/com/ts/tunjave/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -39,42 +42,38 @@ import ly.img.android.serializer._3.IMGLYFileWriter;
 public class MainActivity extends  Activity {
 
     Button openGallery;
+    Button openCamera;
 
     public static int PESDK_RESULT = 1;
     public static int GALLERY_RESULT = 2;
+    public static int CAMERA_RESULT = 3;
+
 
     private SettingsList createPesdkSettingsList() {
-
         // Create a empty new SettingsList and apply the changes on this reference.
         PhotoEditorSettingsList settingsList = new PhotoEditorSettingsList();
-
         // If you include our asset Packs and you use our UI you also need to add them to the UI Config,
         // otherwise they are only available for the backend
         // See the specific feature sections of our guides if you want to know how to add your own Assets.
-
         settingsList.getSettingsModel(UiConfigFilter.class).setFilterList(
                 FilterPackBasic.getFilterPack()
         );
-
         settingsList.getSettingsModel(UiConfigText.class).setFontList(
                 FontPackBasic.getFontPack()
         );
-
         settingsList.getSettingsModel(UiConfigFrame.class).setFrameList(
                 FramePackBasic.getFramePack()
         );
-
         settingsList.getSettingsModel(UiConfigOverlay.class).setOverlayList(
                 OverlayPackBasic.getOverlayPack()
         );
-
         settingsList.getSettingsModel(UiConfigSticker.class).setStickerLists(
                 StickerPackEmoticons.getStickerCategory(),
                 StickerPackShapes.getStickerCategory()
         );
-
         return settingsList;
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,12 +81,21 @@ public class MainActivity extends  Activity {
         setContentView(R.layout.activity_main);
 
         openGallery = findViewById(R.id.openGallery);
+        openCamera = findViewById(R.id.openCamera);
+
         openGallery.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 openSystemGalleryToSelectAnImage();
             }
         });
+
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openSystemCameraToTakeAnImage();
+            }
+        });
     }
+
 
     private void openSystemGalleryToSelectAnImage() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -98,6 +106,12 @@ public class MainActivity extends  Activity {
         }
     }
 
+    private void openSystemCameraToTakeAnImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
+        startActivityForResult(intent, CAMERA_RESULT);
+    }
+
+
     private void openEditor(Uri inputImage) {
         SettingsList settingsList = createPesdkSettingsList();
 
@@ -106,28 +120,43 @@ public class MainActivity extends  Activity {
 
         settingsList.getSettingsModel(PhotoEditorSaveSettings.class).setOutputToGallery(Environment.DIRECTORY_DCIM);
 
-        new PhotoEditorBuilder(this)
-                .setSettingsList(settingsList)
-                .startActivityForResult(this, PESDK_RESULT);
+        new PhotoEditorBuilder(this).setSettingsList(settingsList).startActivityForResult(this, PESDK_RESULT);
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
+
         if (resultCode == RESULT_OK && requestCode == GALLERY_RESULT) {
             // Open Editor with some uri in this case with an image selected from the system gallery.
             Uri selectedImage = intent.getData();
             openEditor(selectedImage);
 
+        } else if (resultCode == RESULT_OK && requestCode == CAMERA_RESULT) {
+            Bundle extras = intent.getExtras();
+            Bitmap bp = (Bitmap) extras.get("data");
+            Uri capturedImage = getImageUri(this, bp);
+            openEditor(capturedImage);
+
         } else if (resultCode == RESULT_OK && requestCode == PESDK_RESULT) {
             // Editor has saved an Image.
             EditorSDKResult data = new EditorSDKResult(intent);
 
+
             Log.i("PESDK", "Source image is located here " + data.getSourceUri());
             Log.i("PESDK", "Result image is located here " + data.getResultUri());
 
+
             // TODO: Do something with the result image
+
 
             // OPTIONAL: read the latest state to save it as a serialisation
             SettingsList lastState = data.getSettingsList();
@@ -136,11 +165,15 @@ public class MainActivity extends  Activity {
                         getExternalFilesDir(null),
                         "serialisationReadyToReadWithPESDKFileReader.json"
                 ));
-            } catch (IOException e) { e.printStackTrace(); }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
 
         } else if (resultCode == RESULT_CANCELED && requestCode == PESDK_RESULT) {
             // Editor was canceled
             EditorSDKResult data = new EditorSDKResult(intent);
+
 
             Uri sourceURI = data.getSourceUri();
             // TODO: Do something with the source...
